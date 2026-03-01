092634d05c8366db2b4e890af0baeadcced94bc4
Khushi <sainikhushi2229@gmail.com>
2026-03-01 15:49:37 +0530

    Refactor game start logic to use start button

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ let started = false;
 
 let colors = ["red", "blue", "green", "yellow"];
 
-document.addEventListener("keypress", function () {
-    if (started === false) {
+let startBtn = document.querySelector("#start-btn");
+
+startBtn.addEventListener("click", function () {
+    if (!started) {
         levelUp();
         started = true;
     }
@@ -65,4 +67,5 @@ function checkUserSequence() {
 let btns = document.querySelectorAll(".color-button");
 for (btn of btns) {
     btn.addEventListener("click", userPressButton);
+
 }
